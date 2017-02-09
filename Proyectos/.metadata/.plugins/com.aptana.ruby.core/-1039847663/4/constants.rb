47a4aff225beeffde8b82c2b5c601e3f681e7852
xDrums <lush.barrera@gmail.com>
2017-02-09 09:40:03 -0300

    blog

---
+++ Proyectos/.metadata/.plugins/com.aptana.ruby.core/-1039847663/4/constants.rb
@@ -0,0 +1,13 @@
+ARGV = []
+TRUE = true
+FALSE = false
+STDOUT = IO.new
+STDIN = IO.new
+STDERR = IO.new
+ENV = {}
+NIL = nil
+RUBY_PATCHLEVEL = 222
+RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2016 Yukihiro Matsumoto'
+TOPLEVEL_BINDING = Binding.new
+RUBY_VERSION = '2.3.3'
+RUBY_PLATFORM = 'i386-mingw32'
