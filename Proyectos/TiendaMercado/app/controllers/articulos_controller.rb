ab48b7362b7f2e60ef5f8e0775b04c2f8b001bbc
xDrums <lush.barrera@gmail.com>
2017-03-30 16:53:23 -0300

    Seguridad en views y correccion de sesiones

--- Proyectos/TiendaMercado/app/controllers/articulos_controller.rb
+++ Proyectos/TiendaMercado/app/controllers/articulos_controller.rb
@@ -1,6 +1,6 @@
 class ArticulosController < ApplicationController
-  before_action :set_articulo, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_articulo, only: [:show, :update, :destroy]
+ before_action :authenticate_user!,except:[:index, :show]
   # GET /articulos
   # GET /articulos.json
   def index
--- Proyectos/TiendaMercado/config/routes.rb
+++ Proyectos/TiendaMercado/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articulos 
   
+  #ruta para el localhost:3000
+  root 'articulos#index' 
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
